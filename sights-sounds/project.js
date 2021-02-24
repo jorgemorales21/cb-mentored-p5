512f55f6cf52aedfc978c651846c8d77ec63f198
Jorge Morales <jorge.morales@berkeley.edu>
2021-02-23 20:42:44 -0800

    Resized to fit entire viewport and not have to scroll vertically/horizontally

--- sights-sounds/project.js
+++ sights-sounds/project.js
@@ -22,12 +22,15 @@ var campanileIndex;
 var campanilePicButton;
 var haasPic;
 var endPic;
+var frisbeePic;
+var snakePic;
+var snakesButton;
 
 var trajan;
 var fightSong;
 var buttonPressSound;
-var canvasX = 1750; // Normal Ratio (full-screen): 1500
-var canvasY = 1000; // Normal Ratio (full-screen): 1000
+var canvasX = 1440; // Normal Ratio (full-screen): 1500
+var canvasY = 830; // Normal Ratio (full-screen): 1000
 var startButton;
 var nextButton;
 var tourStops;
@@ -45,6 +48,7 @@ function preload() {
   moffitt3 = loadImage('media/moffitt-3.png');
   moffittPics = [moffitt1, moffitt2, moffitt3];
   memorialGladePic = loadImage('media/memorial-glade.jpg');
+  frisbeePic = loadImage('media/frisbee.png');
   doeLibraryPic = loadImage('media/doe-library.png');
   mainStacksPic = loadImage('media/main-stacks.jpg');
   campanile1 = loadImage('media/campanile-1.jpg');
@@ -52,6 +56,7 @@ function preload() {
   campanile3 = loadImage('media/campanile-3.jpg');
   campanile4 = loadImage('media/campanile-4.jpg');
   campanile5 = loadImage('media/campanile-5.png');
+  snakePic = loadImage('media/snake.png');
   campanilePics = [campanile1, campanile2, campanile3, campanile4, campanile5];
   haasPic = loadImage('media/haas.jpg');
   memorialStadiumPic = loadImage('media/memorial-stadium.jpg');
@@ -75,11 +80,15 @@ function setup() {
 
 function draw() {
   cursor('media/oski.png');
+  if (upcomingCallback == doeLibrary) {
+    //image(frisbeePic, mouseX, mouseY, 140, 140);
+    cursor('media/114.png');
+  }
 }
 
 function introScreen() {
   image(introPic, 0, 0, canvasX, canvasY);
-  textFont(trajan, 100);
+  textFont(trajan, 110);
   fill(color('#FDB515'));
   text('Sights & Sounds', 300, 150);
   textSize(50);
@@ -108,7 +117,7 @@ function playButtonSound() {
 function createNextButton() {
   nextButton = createButton('Next Stop');
   nextButton.size(150, 50);
-  nextButton.position(1530, 900);
+  nextButton.position(1250, 630);
   nextButton.mousePressed(amazonStore);
   upcomingCallback = amazonStore;
 }
@@ -124,11 +133,11 @@ function satherGate() {
 function satherGateText() {
   textFont('arial', 100);
   fill(color('#FFFFFF'));
-  text('Welcome to Berkeley!', 400, 110);
-  textSize(50);
+  text('Welcome to Berkeley!', 250, 110);
+  textSize(47);
   fill(color('#003262'));
   text(`This is Sather Gate. Since its construction in 1910, Sather
-  Gate has served as the entrance to the heart of campus`, 190, 870);
+  Gate has served as the entrance to the heart of campus`, 85, 730);
 }
 
 function amazonStore() {
@@ -142,12 +151,12 @@ function amazonStore() {
 function amazonStoreText() {
   fill(color('#ED4E33'));
   textSize(150);
-  text('Amazon Store', 500, 190);
+  text('Amazon Store', 250, 190);
   textSize(50);
   fill(color('#00B0DA'));
   text(`Welcome to the Amazon Store in MLK.
   Here you can pick up all your Amazon
-  packages in one of the 6 self-serve lockers.`, 500, 800);
+  packages in one of the 6 self-serve lockers.`, 260, 660);
 }
 
 function upperSproul() {
@@ -161,13 +170,13 @@ function upperSproul() {
 function upperSproulText() {
   fill(color('#00A598'));
   textSize(150);
-  text('Sproul', 600, 190);
-  textSize(50);
+  text('Sproul', 500, 190);
+  textSize(40);
   fill(color('#DDD5C7'));
   text(`Next up is Upper Sproul. Home to Berkeley's
   many protests, including the FSM demonstrations.
   Be sure to check out all the student orgs tabling here.
-  Oh, and good luck avoiding the flyers!`, 200, 750);
+  Oh, and good luck avoiding the flyers!`, 160, 630);
 }
 
 function moffitt() {
@@ -178,7 +187,7 @@ function moffitt() {
   moffittText();
   picChangeButton = createButton('>>>');
   picChangeButton.size(150, 50);
-  picChangeButton.position(1530, 840);
+  picChangeButton.position(1250, 575);
   picChangeButton.mousePressed(moffittPicChange);
   moffittIndex = 1;
 }
@@ -195,13 +204,13 @@ function moffittPicChange() {
 
 function moffittText() {
   fill(color('#C4820E'));
-  textSize(150);
-  text('Moffitt Library', 500, 200);
-  textSize(50);
+  textSize(120);
+  text('Moffitt Library', 380, 166);
+  textSize(45);
   fill(color('#00A598'));
   text(`You've arrived at Moffitt Library. Unlike most other
   libraries, Moffitt allows eating, drinking, socializing,
-  and even sleeping!`, 250, 840);
+  and even sleeping!`, 170, 670);
 }
 
 function memorialGlade() {
@@ -215,14 +224,14 @@ function memorialGlade() {
 
 function memorialGladeText() {
   fill(color('#ED4E33'));
-  textSize(150);
+  textSize(135);
   text(`Memorial
-  Glade`, 150, 200);
-  textSize(50);
+  Glade`, 40, 170);
+  textSize(45);
   fill(color('#FFFFFF'));
-  text(`If you love playing spikeball, throwing a frisbee,
-  or having a nice picnic on a sunny day, then Memorial Glade
-  is the spot for you!`, 80, 750);
+  text(`If you love playing spikeball, throwing a frisbee, or having 
+  a nice picnic on a sunny day, then Memorial Glade is 
+  the spot for you!`, 50, 650);
 }
 
 function doeLibrary() {
@@ -235,13 +244,14 @@ function doeLibrary() {
 
 function doeLibraryText() {
   fill(color('#3B7EA1'));
-  textSize(150);
-  text(`Doe Library`, 150, 200);
-  textSize(50);
+  textSize(130);
+  text(`Doe Library`, 280, 135);
+  textSize(43);
   fill(color('#FFFFFF'));
-  text(`Doe Library: The perfect spot to get some work done
-  in between classes. Seeing everyone else studying is sure
-  to boost your productivity.`, 300, 300);
+  text(`Doe Library: The perfect spot to get some
+  work done in between classes. Seeing
+  everyone else studying is sure
+  to boost your                productivity.`, 420, 200);
 }
 
 function mainStacks() {
@@ -254,14 +264,14 @@ function mainStacks() {
 
 function mainStacksText() {
   fill(color('#00A598'));
-  textSize(150);
-  text(`Main Stacks`, 700, 200);
-  textSize(50);
+  textSize(120);
+  text(`Main Stacks`, 510, 140);
+  textSize(40);
   fill(color('#46535E'));
-  text(`If you prefer studying
-  underground so no one can
-  hear you crying, then you've
-  gotta come to main stacks!`, 800, 300);
+  text(`If you prefer
+  studying underground so no one can
+  hear you crying, then you've gotta
+            come to main stacks!`, 570, 190);
 }
 
 function campanile() {
@@ -272,23 +282,23 @@ function campanile() {
   campanileText();
   campanilePicButton = createButton('>>>');
   campanilePicButton.size(150, 50);
-  campanilePicButton.position(1530, 840);
+  campanilePicButton.position(1250, 575);
   campanilePicButton.mousePressed(campanilePicChange);
   campanileIndex = 0;
 }
 
 function campanileText() {
   fill(color('#00A598'));
-  textSize(150);
-  text(`Campanile`, 830, 130);
-  textSize(50);
+  textSize(120);
+  text(`Campanile`, 685, 130);
+  textSize(35);
   fill(color('#003262'));
   text(`Behold our most beloved
-  structure: The Campanile,
+  structure: The Campanile—
   taller than Stanfurd's
   Hoover Tower and the
   3rd tallest bell tower
-  in the world!`, 840, 200);
+  in the world!`, 710, 190);
 }
 
 function campanilePicChange() {
@@ -308,18 +318,33 @@ function haas() {
   nextButton.mousePressed(memorialStadium);
   upcomingCallback = memorialStadium;
   haasText();
+  snakesButton = createButton('Reveal Snakes');
+  snakesButton.size(150, 50);
+  snakesButton.position(1250, 575);
+  snakesButton.mousePressed(revealSnakes);
+}
+
+function revealSnakes() {
+  image(snakePic, 40, 70, 100, 100);
+  image(snakePic, 20, 500, 100, 100);
+  image(snakePic, 90, 580, 100, 100);
+
+  image(snakePic, 700, 650, 100, 100);
+  image(snakePic, 1000, 690, 100, 100);
+  image(snakePic, 1100, 710, 100, 100);
 }
 
 function haasText() {
   fill(color('#00A598'));
-  textSize(100);
-  text(`Haas School of Business`, 500, 100);
-  textSize(40);
+  textSize(80);
+  text(`Haas School of Business`, 420, 100);
+  textSize(27);
   fill(color('#003262'));
-  text(`Watch out for the snakes!`, 1130, 140);
+  text(`Watch out for the snakes!`, 990, 130);
 }
 
 function memorialStadium() {
+  snakesButton.hide();
   playButtonSound();
   image(memorialStadiumPic, 0, 0, canvasX, canvasY);
   nextButton.mousePressed(end);
@@ -329,13 +354,13 @@ function memorialStadium() {
 
 function memorialStadiumText() {
   fill(color('#FFFFFF'));
-  textSize(150);
-  text(`Memorial Stadium`, 300, 130);
-  textSize(50);
+  textSize(140);
+  text(`Memorial Stadium`, 170, 130);
+  textSize(25);
   fill(color('#FDB515'));
   text(`Home to Cal Football, Memorial Stadium
   sits atop the Hayward Fault, making for
-  some pretty shaky football games.`, 400, 200);
+  some pretty shaky football games.`, 410, 180);
 }
 
 function end() {
@@ -349,13 +374,13 @@ function end() {
 function endText() {
   fill(color('#3B7EA1'));
   textSize(150);
-  text(`The End`, 250, 150);
-  textSize(50);
+  text(`The End`, 250, 130);
+  textSize(30);
   fill(color('#00A598'));
   text(`And that concludes our campus tour. Don't forget to stop by
-  the Cal Student Store and pick up your favorite merch!`, 355, 220);
+  the Cal Student Store and pick up your favorite merch!`, 400, 180);
   fill(color('#6C3302'));
-  text(`Thanks for visiting!`, 1100, 350);
+  text(`Thanks for visiting!`, 800, 260);
 }
 
 function keyPressed() {
